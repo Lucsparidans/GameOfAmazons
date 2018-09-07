1df207ec54b84988ffedbba581b5cda7d6de2be1
lucsparidans <lucsnadiraps@gmail.com>
2018-09-07 17:20:24 +0200

    Config settings

--- desktop/src/com/dke/game/desktop/DesktopLauncher.java
+++ desktop/src/com/dke/game/desktop/DesktopLauncher.java
@@ -5,8 +5,24 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.dke.game.View.View;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new View(), config);
-	}
+    private static final int width = 1920;
+    private static final int height = 1200;
+    public static boolean fullScreen = false;   //Fullscreen is really buggy...
+    private static LwjglApplicationConfiguration config;
+
+    public static void main(String[] arg) {
+        config = new LwjglApplicationConfiguration();
+        config.fullscreen = fullScreen;
+        config.title = "Game of Amazons";
+        config.resizable = false;
+        if(!fullScreen){
+            setSize();
+        }
+        new LwjglApplication(new View(), config);
+    }
+
+    private static void setSize() {
+        config.width = width;
+        config.height = height;
+    }
 }
