76ad03995710d4ff2fcb72a02d267397481bd50a
lucsparidans <lucsnadiraps@gmail.com>
2018-09-07 15:45:57 +0200

    Initial commit

---
+++ desktop/src/com/dke/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dke.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dke.game.Controller;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Controller(), config);
+	}
+}
