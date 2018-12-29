b80851ec301668c41b18b5ae5b08af65cb28e2d7
Casteeuwen <35225641+Casteeuwen@users.noreply.github.com>
2018-12-29 02:20:40 +0100

    Added CarloCoordinate

---
+++ core/src/com/dke/game/Models/AI/Luc/MyAlgo/CarloCoordinate.java
@@ -0,0 +1,12 @@
+package com.dke.game.Models.AI.Luc.MyAlgo;
+
+public class CarloCoordinate {
+    int x, y;
+    public CarloCoordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){return x;}
+    public int getY(){return y;}
+}
